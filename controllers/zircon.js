b7c63de3b64812875513f6d61d52002b3c1e2bf2
Jacen Piatt <jacenpiatt@gmail.com>
2025-04-10 21:34:25 -0500

    Lab 12

--- controllers/zircon.js
+++ controllers/zircon.js
@@ -26,9 +26,17 @@ exports.zircon_view_all_Page = async function(req, res) {
     };
 
 // for a specific zircon.
-exports.zircon_detail = function(req, res) {
-res.send('NOT IMPLEMENTED: Zircon detail: ' + req.params.id);
-};
+exports.zircon_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await Zircon.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+    };
+    
 // Handle zircon create on POST.
 exports.zircon_create_post = async function(req, res) {
 console.log(req.body)
@@ -37,9 +45,9 @@ let document = new Zircon();
 // Even though bodies can be in many different formats, we will be picky
 // and require that it be a json object
 // {"zircon_type":"goat", "cost":12, "size":"large"}
-document.zircon_type = req.body.purity;
-document.cost = req.body.origin;
-document.size = req.body.hardness;
+document.purity = req.body.purity;
+document.origin = req.body.origin;
+document.hardness = req.body.hardness;
 try{
 let result = await document.save();
 res.send(result);
@@ -53,7 +61,23 @@ res.send(`{"error": ${err}}`);
 exports.zircon_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: Zircon delete DELETE ' + req.params.id);
 };
-// Handle zircon update form on PUT.
-exports.zircon_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: Zircon update PUT' + req.params.id);
+//Handle Zircon update form on PUT.
+exports.zircon_update_put = async function(req, res) {
+console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+try {
+let toUpdate = await Zircon.findById( req.params.id)
+// Do updates of properties
+if(req.body.purity)
+toUpdate.purity = req.body.purity;
+if(req.body.origin) toUpdate.origin = req.body.origin;
+if(req.body.hardness) toUpdate.hardness = req.body.hardness;
+let result = await toUpdate.save();
+console.log("Sucess " + result)
+res.send(result)
+} catch (err) {
+res.status(500)
+res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+}
 };
