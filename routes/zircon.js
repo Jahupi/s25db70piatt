628f3fa5b75d9594fb652675aacede2945a55b24
Jacen Piatt <jacenpiatt@gmail.com>
2025-04-14 11:55:54 -0500

    Part 4.6

--- routes/zircon.js
+++ routes/zircon.js
@@ -3,5 +3,8 @@ const zircon_controlers = require('../controllers/zircon');
 var router = express.Router();
 /* GET zircons */
 router.get('/', zircon_controlers.zircon_view_all_Page );
+
+/* GET detail zircon page */
 router.get('/detail', zircon_controlers.zircon_view_one_Page);
+
 module.exports = router;
