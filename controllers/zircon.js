2139ee5c049f1e19027aa74cf41ddd7d5510c9ee
Jacen Piatt <jacenpiatt@gmail.com>
2025-04-14 11:14:56 -0500

    Part 4.1

--- controllers/zircon.js
+++ controllers/zircon.js
@@ -70,7 +70,7 @@ exports.zircon_delete = async function(req, res) {
     res.status(500)
     res.send(`{"error": Error deleting ${err}}`);
     }
-    }
+    };
 
 
 //Handle Zircon update form on PUT.
@@ -93,3 +93,19 @@ res.send(`{"error": ${err}: Update for id ${req.params.id}
 failed`);
 }
 };
+
+
+
+// Handle a show one view with id specified by query
+exports.zircon_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+    result = await Zircon.findById( req.query.id)
+    res.render('zircondetail',
+    { title: 'Zircon Detail', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
--- routes/zircon.js
+++ routes/zircon.js
@@ -3,4 +3,5 @@ const zircon_controlers = require('../controllers/zircon');
 var router = express.Router();
 /* GET zircons */
 router.get('/', zircon_controlers.zircon_view_all_Page );
+router.get('/detail', zircon_controlers.zircon_view_one_Page);
 module.exports = router;
