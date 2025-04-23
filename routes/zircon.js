6c1cd89b90ccc603731d0c038c8b228d500412c1
Jacen Piatt <jacenpiatt@gmail.com>
2025-04-23 11:26:47 -0500

    Part 3 and 4

--- routes/zircon.js
+++ routes/zircon.js
@@ -1,6 +1,17 @@
 var express = require('express');
 const zircon_controlers = require('../controllers/zircon');
 var router = express.Router();
+
+// A little function to check if we have an authorized user and continue on
+//or
+// redirect to login.
+const secured = (req, res, next) => {
+if (req.user){
+return next();
+}
+res.redirect("/login");
+}
+
 /* GET zircons */
 router.get('/', zircon_controlers.zircon_view_all_Page );
 
@@ -10,8 +21,10 @@ router.get('/detail', zircon_controlers.zircon_view_one_Page);
 /* GET create zircon page */
 router.get('/create', zircon_controlers.zircon_create_Page);
 
-/* GET create update page */
-router.get('/update', zircon_controlers.zircon_update_Page);
+/* GET update costume page */
+router.get('/update', secured,
+zircon_controlers.zircon_update_Page);
+
 
 /* GET delete zircon page */
 router.get('/delete', zircon_controlers.zircon_delete_Page);
